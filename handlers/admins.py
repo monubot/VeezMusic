0682a919da4481a962645602e825cfbb02722672
Nama Saya <86531458+monubot@users.noreply.github.com>
2021-09-16 00:11:51 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,17 +1,27 @@
 # Copyright (C) 2021 VeezMusicProject
 
 
+import traceback
+import asyncio
 from asyncio import QueueEmpty
 from config import que
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery, ChatPermissions
 
 from cache.admins import admins
 from helpers.channelmusic import get_chat_id
 from helpers.decorators import authorized_users_only, errors
+from handlers.play import cb_admin_check
 from helpers.filters import command, other_filters
 from callsmusic import callsmusic
 from callsmusic.queues import queues
+from config import LOG_CHANNEL, OWNER_ID, BOT_USERNAME, COMMAND_PREFIXES
+from helpers.database import db, dcmdb, Database
+from helpers.dbtools import handle_user_status, delcmd_is_on, delcmd_on, delcmd_off
+from helpers.helper_functions.admin_check import admin_check
+from helpers.helper_functions.extract_user import extract_user
+from helpers.helper_functions.string_handling import extract_time
+
 
 
 @Client.on_message(filters.command("reload"))
