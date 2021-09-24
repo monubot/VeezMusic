068523f4631d1b4d84d428d9b6a90ea00bb3c340
Nama Saya <86531458+monubot@users.noreply.github.com>
2021-09-25 02:06:23 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -17,7 +17,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-            f"✨ Hello, i'm a official **music assistant of {BOT_NAME}.**\n\n❗️ **notes:**\n\n⫸ don't spam message.\n⫸ don't send me anything confidential\n\n⨀ Join to @{UPDATES_CHANNEL}\n\n👩🏻‍💻 Dev: @{OWNER_NAME}\n\n",
+            f"✨ Hello, i'm a official **music assistant of {BOT_NAME}.**\n\n❗️ **notes:**\n⫸ Don't spam message.\n⫸ Don't send me anything confidential\n\n⨀ Join to @{UPDATES_CHANNEL}\n\n👩🏻‍💻 Dev: @{OWNER_NAME}\n\n",
             )
             return
 
